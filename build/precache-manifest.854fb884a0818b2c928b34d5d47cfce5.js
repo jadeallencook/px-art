d65232d92a2c49b475c84ac2a12bc8c8eb3145ac
Jade Allen Cook <jadeallencook@gmail.com>
2020-06-20 13:33:10 -0700

    exported build

---
+++ build/precache-manifest.854fb884a0818b2c928b34d5d47cfce5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cab3dd0d444e0fd08c968b9cfa47bd",
+    "url": "/px-art/build/index.html"
+  },
+  {
+    "revision": "f240bd6e167a76ede95d",
+    "url": "/px-art/build/static/css/main.b69c4953.chunk.css"
+  },
+  {
+    "revision": "d893dc7eb31dc961f764",
+    "url": "/px-art/build/static/js/2.f5c4d015.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/px-art/build/static/js/2.f5c4d015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f240bd6e167a76ede95d",
+    "url": "/px-art/build/static/js/main.5c47135a.chunk.js"
+  },
+  {
+    "revision": "25d8200062a17c88b358",
+    "url": "/px-art/build/static/js/runtime-main.db06f73c.js"
+  },
+  {
+    "revision": "6d228f293d12439067e28abd1bfcaf2b",
+    "url": "/px-art/build/static/media/click.6d228f29.wav"
+  },
+  {
+    "revision": "9f6c9dfe5ff4a191aafafaf7dc58d0cb",
+    "url": "/px-art/build/static/media/pop.9f6c9dfe.wav"
+  }
+]);
